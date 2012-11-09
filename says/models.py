4eebb44e872fa45939577725b01795eea86f7eea
Angel Medrano <asmedrano@gmail.com>
2012-11-09 13:08:37 -0500

    initial commit of cookies

--- says/models.py
+++ says/models.py
@@ -2,11 +2,17 @@ from django.db import models
 
 
 class Message(models.Model):
+    MSSG_TYPE_CHOICES = (
+        ('notification', 'Notification'),
+        ('persistant','Persistant'),
+    )
+
     title = models.CharField(max_length=200, blank=False)
     slug = models.SlugField(max_length=200, blank=False)
     message = models.TextField(blank=False)
+    message_type = models.CharField(max_length=30, choices=MSSG_TYPE_CHOICES, default='notification')
     duration_start = models.DateField(blank=True,null=True, help_text='Optional message to when this message should begin displaying') # optional
     duration_end = models.DateField(blank=True, null=True, help_text='Optional message to when this message should end displaying') # optional
 
     def __unicode__(self):
-        return self.title
+        return "Type: %s -- %s" % (self.message_type, self.title)
--- says/templatetags/says_extras.py
+++ says/templatetags/says_extras.py
@@ -1,17 +1,31 @@
 from datetime import datetime
 from datetime import timedelta
 from django import template
+from django.conf import settings
 from says.models import Message
 
 register = template.Library()
 
+USE_DEFAULT_CLOSE = getattr(settings,'USE_DEFAULT_CLOSE',True)
 
 @register.inclusion_tag('messages.html')
 def get_messages():
     today = datetime.now()
-    mssgs = []
-    # get all the messages with no time exipration
-    mssgs += Message.objects.filter(duration_start=None, duration_end=None)
-    # now get all messages that are not expired.
-    mssgs += Message.objects.filter(duration_start__gte=today, duration_end__gte=today)
-    return {'mssgs':mssgs}
+    has_mssgs = False
+    # get persistant messages
+    p_mssgs = []
+    # get all the notifications with no time exipration
+    p_mssgs += Message.objects.filter(duration_start=None, duration_end=None, message_type='persistant')
+    # now get all notifications that are not expired.
+    p_mssgs += Message.objects.filter(duration_start__gte=today, duration_end__gte=today, message_type='persistant')
+
+    notifications = []
+    # get all the notifications with no time exipration
+    notifications += Message.objects.filter(duration_start=None, duration_end=None, message_type='notification')
+    # now get all notifications that are not expired.
+    notifications += Message.objects.filter(duration_start__gte=today, duration_end__gte=today, message_type='notification')
+
+    if len(notifications) or len(p_mssgs):
+        has_mssgs = True
+
+    return {'has_mssgs':has_mssgs, 'notices':notifications, 'persistant':p_mssgs, 'default_close': USE_DEFAULT_CLOSE}
